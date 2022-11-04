576e6fdff47095cd00e3283ed7bc11ef49d7ef38
2741140081 <2741140081@qq.com>
2022-11-04 14:12:23 +0800

    update second LeetCode975.java

--- edms/src/main/java/com/marks/edms/leetcode/LeetCode975.java
+++ edms/src/main/java/com/marks/edms/leetcode/LeetCode975.java
@@ -3,6 +3,6 @@ package com.marks.edms.leetcode;
 public class LeetCode975 {
     public void addSomeThing(int[] arr) {
         System.out.println("测试");
-
+        System.out.println("测试2");
     }
 }
