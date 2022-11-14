19c24cc51ef07760358ec6a2e074a97b6ab6f7f9
2741140081 <2741140081@qq.com>
2022-11-14 10:04:24 +0800

    upload the same name file

--- edms/src/main/java/com/marks/edms/controller/UploadController.java
+++ edms/src/main/java/com/marks/edms/controller/UploadController.java
@@ -1,11 +1,9 @@
 package com.marks.edms.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -28,7 +26,7 @@ public class UploadController {
         String filename = file.getOriginalFilename();
         String suffixname = filename.substring(filename.lastIndexOf("."));
 
-//        生成文件名称通用方法
+        //生成文件名称通用方法
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
         Random r = new Random();
         StringBuilder tempName = new StringBuilder();
@@ -46,5 +44,48 @@ public class UploadController {
         return "上传成功，地址为: /upload/"+newFileName;
 
     }
+
+    @RequestMapping(value = "/uploadFilesBySameName", method = RequestMethod.POST)
+    @ResponseBody
+    public String uploadFilesBySameName(@RequestPart MultipartFile[] files) {
+        if (files == null || files.length == 0) {
+            return "参数异常";
+        }
+        if (files.length > 5) {
+            return "最多上传5个文件";
+        }
+        String uploadResult = "文件上传成功，地址为";
+
+        for (MultipartFile file : files) {
+            String filename = file.getOriginalFilename();
+            if (StringUtils.isEmpty(filename)) {
+                //表示无文件信息
+                continue;
+            }
+
+            String suffixname = filename.substring(filename.lastIndexOf("."));
+
+            //生成文件名称通用方法
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            Random r = new Random();
+            StringBuilder tempName = new StringBuilder();
+            tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixname);
+
+            String newFileName = tempName.toString();
+
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(FILE_UPLOAD_PATH + newFileName);
+                Files.write(path,bytes);
+                uploadResult += "/upload/" + newFileName +"<br>";
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+        return uploadResult;
+    }
+
 
 }
