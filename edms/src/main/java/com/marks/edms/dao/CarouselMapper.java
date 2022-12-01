751028261cc2dece4175454ae9ecd15f935f6fd5
2741140081 <2741140081@qq.com>
2022-12-01 15:25:27 +0800

    upload the index-all.html

--- edms/src/main/java/com/marks/edms/dao/CarouselMapper.java
+++ edms/src/main/java/com/marks/edms/dao/CarouselMapper.java
@@ -2,6 +2,7 @@ package com.marks.edms.dao;
 
 import com.marks.edms.entity.Carousel;
 import com.marks.edms.util.PageQueryUtil;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -49,7 +50,33 @@ public interface CarouselMapper {
      */
     int updateByPrimaryKey(Carousel carousel);
 
+    /**
+     * 查询分页数据
+     * @param pageQueryUtil
+     * @return
+     */
     List<Carousel> findCarouselList(PageQueryUtil pageQueryUtil);
+
+    /**
+     * 获取记录总数
+     * @param pageQueryUtil
+     * @return
+     */
+    int getTotalCarousels(PageQueryUtil pageQueryUtil);
+
+    /**
+     * 批量删除
+     * @param Ids
+     * @return
+     */
+    int deleteBath(Integer[] Ids);
+
+    /**
+     * 查询固定数量的记录
+     * @param number
+     * @return
+     */
+    List<Carousel> findCarouselByNum(@Param("number") int number);
 
 
 }
