e936927237dbd3a5a4c355fcb1f3b57515f4c3dd
2741140081 <2741140081@qq.com>
2022-12-07 09:28:59 +0800

    轮播图后端代码

---
+++ edms/src/main/java/com/marks/edms/leetcode/LeetCode1805.java
@@ -0,0 +1,52 @@
+package com.marks.edms.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode1805 {
+    public static void main(String[] args) {
+        String word = "a123bc34d008e8f34";
+        String word1 = "0000001110000000111";
+        String str = word1.replaceFirst("^0*","");
+
+        numDifferentInteger(word);
+    }
+
+    private static int numDifferentInteger(String word) {
+        if (word.length() < 1) {
+            return 0;
+        }
+        Set<String> numList = new HashSet<>();
+        char[] chars = word.toCharArray();
+        String temp = "";
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 48 && chars[i] <=57) {
+                temp += chars[i];
+            }else {
+                temp += ",";
+            }
+        }
+        String[] strings = temp.split(",");
+
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() > 0) {
+                String tempString = removePreZero(strings[i]);
+                numList.add(tempString);
+            }
+        }
+
+        return numList.size();
+    }
+
+    private static String removePreZero(String targetString) {
+        int p2 = targetString.length();
+        int p1 = 0;
+        while (p2 - p1 >1 && targetString.charAt(p1) == '0') {
+            p1++;
+        }
+        targetString = targetString.substring(p1,p2);
+        return targetString;
+
+    }
+}
