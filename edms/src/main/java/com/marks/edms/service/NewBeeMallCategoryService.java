71a365bda867c64f4c64f3e437f46f490e3a13a8
2741140081 <2741140081@qq.com>
2023-01-03 15:11:57 +0800

    轮播图后端代码

---
+++ edms/src/main/java/com/marks/edms/service/NewBeeMallCategoryService.java
@@ -0,0 +1,22 @@
+package com.marks.edms.service;
+
+import com.marks.edms.entity.GoodsCategory;
+import com.marks.edms.util.PageQueryUtil;
+import com.marks.edms.util.PageResult;
+
+import java.util.List;
+
+public interface NewBeeMallCategoryService {
+
+    PageResult getCategorisePage(PageQueryUtil pageQueryUtil);
+
+    String saveCategory(GoodsCategory goodsCategory);
+
+    String updateGoodsCategory(GoodsCategory goodsCategory);
+
+    GoodsCategory getGoodsCategoryById(Long id);
+
+    Boolean deleteBatch(Integer[] ids);
+
+    List<GoodsCategory> selectByLevelAndParentIdsAndNumber(List<Long> parentIds, int categoryLevel);
+}
