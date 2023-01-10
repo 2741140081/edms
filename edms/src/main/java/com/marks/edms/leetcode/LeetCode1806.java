2ef1de46fca7e150ba4f01b2d9608c5b077a96e8
2741140081 <2741140081@qq.com>
2023-01-10 10:47:51 +0800

    轮播图后端代码

--- edms/src/main/java/com/marks/edms/leetcode/LeetCode1806.java
+++ edms/src/main/java/com/marks/edms/leetcode/LeetCode1806.java
@@ -1,7 +1,45 @@
 package com.marks.edms.leetcode;
 
+import java.util.Arrays;
+
 public class LeetCode1806 {
     public static void main(String[] args) {
+        int n = 8;
+        int i = reinitializePermutation(n);
+        System.out.println("需要经过步数===>" + i);
+    }
 
+    private static int reinitializePermutation(int n) {
+        int[] perm = new int[n];
+        int[] original = new int[n];
+        for (int i = 0; i < perm.length; i++) {
+            perm[i] = i;
+            original[i] = i;
+        }
+
+        perm = permStep(perm);
+        int sum = 1;
+        while (!Arrays.equals(perm, original)) {
+            perm = permStep(perm);
+            sum++;
+        }
+        return sum;
+    }
+
+    private static int[] permStep(int[] perm) {
+        int n = perm.length;
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                arr[i] = perm[i/2];
+            }else {
+                arr[i] = perm[n/2 + (i-1)/2];
+            }
+        }
+        perm = arr;
+        for (int i = 0; i < perm.length; i++) {
+            System.out.println("perm["+i+"]的值"+perm[i]);
+        }
+        return perm;
     }
 }
