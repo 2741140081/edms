5944821caf215bd5aea413ed2d23bee2cc7dd436
2741140081 <2741140081@qq.com>
2023-03-01 09:27:08 +0800

    edit newbee_mall_goods_info

--- edms/src/main/java/com/marks/edms/leetcode/LeetCode704.java
+++ edms/src/main/java/com/marks/edms/leetcode/LeetCode704.java
@@ -4,6 +4,8 @@ public class LeetCode704 {
     public static void main(String[] args) {
         int[] nums = {-1,0,3,5,9,12};
         int target = 9;
+//        String a = "" + target;
+//        long b = Integer.parseInt(a);
 
         System.out.println(search1(nums, target));
         System.out.println(search2(nums, target));
