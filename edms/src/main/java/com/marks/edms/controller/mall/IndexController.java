62cf08ae947faed7bc24e9486181030c60022e69
2741140081 <2741140081@qq.com>
2023-03-10 10:47:38 +0800

    edit newbee_mall_goods_info

---
+++ edms/src/main/java/com/marks/edms/controller/mall/IndexController.java
@@ -0,0 +1,15 @@
+package com.marks.edms.controller.mall;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+    @GetMapping({"/index", "/", "/index.html"})
+    public String indexPage(HttpServletRequest request) {
+        return "mall/index";
+    }
+}
